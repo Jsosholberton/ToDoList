97c14b03c96384053b3a31c048ba231999bf0ad9
rubio91147 <rubendariofb@gmail.com>
2023-09-21 12:05:20 -0500

    fronted

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    
+      <h1>fro</h1>
+    
+  )
+}
+
+export default App
---
+++ frontend/src/layouts/AuthLayout.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+
+
+const AuthLayout = () => {
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
+export default AuthLayout
+
